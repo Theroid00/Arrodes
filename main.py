599dc3b8bffb4d1d2233241095f41d2e7f30aa1a
Theroid00 <155978169+Theroid00@users.noreply.github.com>
2024-01-30 19:26:25 +0530

    main.py

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
 
-import Lotm
-from disnake.ext import commands 
-from disnake import Intents
-intents = Intents.default()
+import os
+from disnake.ext import commands
+import disnake
+
+intents = disnake.Intents.default()
 intents.members = True
 intents.message_content = True
+
 sync = commands.CommandSyncFlags()
 sync.all()
 
-
 bot = commands.InteractionBot(reload=True, command_sync_flags=sync)
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+# Get the path to the "cogs" folder
+cogs_folder = os.path.join(os.path.dirname(__file__), 'cogs')
 
-@bot.slash_command(name='hello')
-async def hello(ctx):
-    await ctx.send(f'Hello {ctx.author.name}!')
+# Load all the cogs in the "cogs" folder
+for filename in os.listdir(cogs_folder):
+    if filename.endswith('.py'):
+        cog_name = filename[:-3]  # Remove the file extension
+        cog_module = f'cogs.{cog_name}'
+        bot.load_extension(cog_module)
 
-@bot.slash_command(name = "aliases")
-async def Aliases(ctx , name):
-    await ctx.send("\n".join(Lotm.aliases(name)))
+bot.run('')
 
-@bot.slash_command(name = "pathways")
-async def Pathway(ctx , name):
-    await ctx.send("\n".join(Lotm.pathways(name)))
 
-@bot.slash_command(name = "authorities")
-async def Authority(ctx , name):
-    await ctx.send("\n".join(Lotm.authorities(name)))
-bot.run('MTE4OTYzMzU3MzYxMTkxMzMyOA.G8ZNS-.29d55PcRR7WZB4Y2ihAbSDXxmXhOX-JqEUShRw')
 
 
