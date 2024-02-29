5150890decc86d34d8c87ecd78f302f47b5916fe
Theroid00 <155978169+Theroid00@users.noreply.github.com>
2024-02-29 16:43:11 +0530

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 from disnake.ext import commands
 import disnake
@@ -15,15 +14,13 @@ bot = commands.InteractionBot(reload=True, command_sync_flags=sync)
 # Get the path to the "cogs" folder
 cogs_folder = os.path.join(os.path.dirname(__file__), 'cogs')
 
+
 # Load all the cogs in the "cogs" folder
 for filename in os.listdir(cogs_folder):
     if filename.endswith('.py'):
-        cog_name = filename[:-3]  # Remove the file extension
-        cog_module = f'cogs.{cog_name}'
-        bot.load_extension(cog_module)
+        bot.load_extension(f'cogs.{filename[:-3]}')
+        
+
+
 
 bot.run('')
-
-
-
-
