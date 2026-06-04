8a686b8d56c27c99638dca60a9d684ce26ec75e6
theroid0 <theroid00@gmail.com>
2026-06-04 11:30:30 +0530

    refactor: optimize name formatting and clean up unused files

--- mystic/helpers/misc.py
+++ mystic/helpers/misc.py
@@ -1,33 +1,31 @@
 """Helper functions for miscellaneous tasks"""
-import nltk
-from nltk import pos_tag
 
-#nltk.download('averaged_perceptron_tagger')
+EXCLUDED_WORDS = {
+    "of", "in", "with", "from", "through", "during", "including", "until", 
+    "against", "among", "throughout", "despite", "towards", "upon", 
+    "concerning", "and", "but", "or", "yet", "so", "the", "a", "an", "to", "for", "at", "by", "on"
+}
 
 
 def format_name(text):
     """
-    This function takes a string as input, splits it into words, and capitalizes each word that is not a preposition or conjunction.
+    This function takes a string as input, splits it into words, and capitalizes each word 
+    that is not a preposition, conjunction, or article.
     It then joins the words together with underscores and returns the result.
 
     Parameters:
     text (str): The input string to be formatted.
 
     Returns:
-    str: The formatted string with capitalized words (excluding prepositions and conjunctions) joined by underscores.
+    str: The formatted string with capitalized words (excluding prepositions/conjunctions/articles) joined by underscores.
     """
-    # POS tagging the words in the input string
-    tagged_words = pos_tag(text.split())
-
-    # List of POS tags for words to be excluded
-    excluded_pos_tags = ["IN", "CC"]
-
-    # Capitalizing each word that is not a preposition or conjunction
+    words = text.split()
     capitalized_words = []
-    for word, tag in tagged_words:
-        if tag not in excluded_pos_tags:
-            capitalized_words.append(word.capitalize())
-        else:
+    for word in words:
+        if word.lower() in EXCLUDED_WORDS:
             capitalized_words.append(word.lower())
+        else:
+            capitalized_words.append(word.capitalize())
 
     return "_".join(capitalized_words)
+
